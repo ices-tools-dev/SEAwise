e4da26e09d6a70e288b1954a0283aba4332e712c
Esther Beukhof <estb@aqua.dtu.dk>
2022-07-26 09:23:18 +0200

    plots ICES ASC presentation

--- Systematic Reviews/Analysis Task 4.1/Rscripts/Step6_Produce_output_manuscript.R
+++ Systematic Reviews/Analysis Task 4.1/Rscripts/Step6_Produce_output_manuscript.R
@@ -19,6 +19,7 @@ library(ggplot2)
 library(viridis)
 library(ggsankey)
 library(ggthemes)
+library(dplyr)
 library(splitstackshape)
 library(RColorBrewer)
 
@@ -45,6 +46,155 @@ data$Region                           <- with(data, ifelse(RegionSEAwise %in% c(
                                                                             ifelse(RegionSEAwise %in% c("CS - Mediterranean", "Mediterranean - non CS"),"Mediterranean Sea", RegionSEAwise)))))
 
 
+
+#####################################################################################################################-
+#####################################################################################################################-
+#-----------------------------------------------#
+# General plots ----
+#-----------------------------------------------#
+
+#-----------------------------------------------#
+## Time series across all papers ----
+#-----------------------------------------------#
+
+YearRet                              <- data[,.(NrPaps = length(unique(SW.ID))),
+                                             by = c("Year")]
+YearRet2                             <- matrix(nrow=1,
+                                               ncol=(max(data$Year) - min(data$Year) + 1),
+                                               NA)
+colnames(YearRet2)                   <- as.character(seq(from=min(data$Year), to= max(data$Year), 1)) 
+rownames(YearRet2)                   <- "Retained"
+
+YearRet2["Retained", as.character(YearRet$Year)] <- YearRet$NrPaps
+YearRet2[is.na(YearRet2)]            <- 0
+
+tiff(paste0(outPath, "YearRet.tiff"), width = 1000, height=750, res=100)
+par(mar=c(5,5,2,2))
+plot(x=c(min(YearRet$Year), max(YearRet$Year)), y=c(0, max(YearRet$NrPaps)), type="n", ann=F, axes=F)
+lines(x=as.numeric(colnames(YearRet2)), y=YearRet2["Retained",], type="o", pch=16, lwd=2, cex=1.5)
+axis(1, at=seq(1965, 2025, 5), labels=seq(1965, 2025, 5), cex.axis=1.5)
+axis(2, las=1, cex.axis=1.5)
+axis(1, at=(max(data$Year) - ((max(data$Year) - min(data$Year))/2)), tick=F, line=2, cex.axis=2, "Publication year")
+axis(2, at=(max(YearRet$NrPaps)/2), tick=F, line=2, cex.axis=2, labels="Number of papers")
+dev.off()
+
+
+#-----------------------------------------------#
+## Barplot for ecosystem component ----
+#-----------------------------------------------#
+
+EcoComp                               <- data[, .(NrPaps = length(unique(SW.ID))),
+                                              by = Ecosystem.component_level1]
+EcoComp                               <- EcoComp[order(NrPaps),,]
+
+tiff(paste0(outPath, "EcoComp.tiff"), width=1000, height=750, res=100)
+par(mar=c(5, 15, 2, 2))
+b                                     <- barplot(EcoComp$NrPaps, horiz=TRUE, axes=F, xlim=c(0,230), col=viridis(3)[2])
+box()
+axis(2, at=b, labels=EcoComp$Ecosystem.component_level1 , las=1, cex.axis=2)
+axis(1, at= seq(0,220, 25), labels=seq(0, 220, 25), cex.axis=1.5)
+text(x=180, y=0, paste0("Total number of unique papers: ", length(unique(data$SW.ID))))
+text(x=EcoComp$NrPaps+7, y=b, EcoComp$NrPaps, cex = 1.3)
+axis(1, at=110, tick=F, line=2, label="Number of papers", cex.axis=2)
+dev.off()
+
+
+
+#-----------------------------------------------#
+## Barplot for Response variable categories ----
+#-----------------------------------------------#
+
+# First manually combine Survival and Mortality studies (all as Mortality)
+ResVarCats                            <- data
+ResVarCats$Response.variable_category <- with(ResVarCats, ifelse(Response.variable_category %in% "Survival","Mortality",Response.variable_category))                            
+
+ResVarCats                            <- ResVarCats[, .(NrPaps = length(unique(SW.ID))),
+                                              by = Response.variable_category]
+ResVarCats                            <- ResVarCats[order(NrPaps),,]
+
+tiff(paste0(outPath, "ResVarCats.tiff"), width=1200, height=750, res=100)
+par(mar=c(5, 23, 2, 2))
+b                                     <- barplot(ResVarCats$NrPaps, horiz=TRUE, axes=F, xlim=c(0,265), col=viridis(3)[2])
+box()
+axis(2, at=b, labels=ResVarCats$Response.variable_category, las=1, cex.axis=2)
+axis(1, at= seq(0,250, 25), labels=seq(0, 250, 25), cex.axis=1.5)
+text(x=162, y=0, paste0("Total number of unique papers: ", length(unique(data$SW.ID))), pos=4)
+text(x=ResVarCats$NrPaps+7.5, y=b, ResVarCats$NrPaps, cex = 1.3)
+axis(1, at=125, tick=F, line=2, label="Number of papers", cex.axis=2)
+dev.off()
+
+
+#-----------------------------------------------#
+## Barplot for pressure types ----
+#-----------------------------------------------#
+
+Pressure                              <- data[, .(NrPaps = length(unique(SW.ID))),
+                                              by = Pressure.type]
+Pressure                              <- Pressure[order(NrPaps, decreasing = TRUE),,]
+
+## Vertical bars
+tiff(paste0(outPath, "Pressures.tiff"), width=850, height=1000, res=100)
+par(mar=c(25,7, 2, 2))
+b                                     <- barplot(Pressure$NrPaps, axes=F, ylim=c(0,350), col=viridis(3)[2])
+box()
+axis(1, at=b, labels=Pressure$Pressure.type , las=2, cex.axis=1.7)
+axis(2, at= seq(0,350, 50), labels=seq(0, 350, 50), cex.axis=1.5, las=1)
+text(x=6.3, y=340, paste0("Total number of unique papers: ", length(unique(data$SW.ID))), cex = 1.3)
+text(x=b, y=Pressure$NrPaps+10, Pressure$NrPaps, cex = 1.3)
+# text(x=b, y=max(subdat$NrPaps)*0.035, labels=subdat$NrPaps, cex=1.5, font=3)
+axis(2, at=190, tick=F, line=3, label="Number of papers", cex.axis=2)
+dev.off()
+
+## Horizontal bars
+Pressure                              <- Pressure[order(NrPaps),,]
+
+tiff(paste0(outPath, "Pressures_hor.tiff"), width=1200, height=750, res=100)
+par(mar=c(5, 28, 2, 2))
+b                                     <- barplot(Pressure$NrPaps, horiz=TRUE, axes=F, xlim=c(0,350), col=viridis(3)[2])
+box()
+axis(2, at=b, labels=Pressure$Pressure.type, las=1, cex.axis=2)
+axis(1, at= seq(0,350, 50), labels=seq(0, 350, 50), cex.axis=1.5)
+text(x=192, y=0, paste0("Total number of unique papers: ", length(unique(data$SW.ID))), pos=4)
+text(x=Pressure$NrPaps+12, y=b, Pressure$NrPaps, cex = 1.3)
+axis(1, at=190, tick=F, line=2, label="Number of papers", cex.axis=2)
+dev.off()
+
+
+
+#-----------------------------------------------#
+## Barplots of fishing gears studied - Comm + Recr only ----
+#-----------------------------------------------#
+Gears                                <- data[,.(NrPaps = length(unique(SW.ID))),
+                                             by = c("Fishery.type", "Gear_level1")]
+Gears                                <- subset(Gears, Fishery.type %in% c("Commercial","Recreational"))
+Gears$Gear_level1                    <- ifelse(is.na(Gears$Gear_level1)==T, "Not specified", 
+                                               ifelse(Gears$Gear_level1 == "Hooks_and_lines", "Hooks and Lines", Gears$Gear_level1))
+GearLevels                           <- unique(Gears$Fishery.type)
+
+tiff(paste0(outPath, "GearsStudiedCommRecr.tiff"), width= 1500, height = 1000, res = 100)
+par(mfrow=c(1,2))
+par(mar=c(18,8,7,1))
+for(iType in seq_along(GearLevels)){
+  subdat                             <- Gears[Gears$Fishery.type == GearLevels[iType],]
+  subdat                             <- subdat[order(NrPaps, decreasing=T)]
+  b                                  <- barplot(subdat$NrPaps, axes=F, ylim=c(0, max(subdat$NrPaps*1.05)), col=viridis(3)[c(2,3)][iType])
+  title(GearLevels[iType], cex.main=3, font=2, line=3)
+  title(paste0("(n = ", sum(subdat$NrPaps), ")"), font.main=3, cex.main=2, line=1.5)
+  axis(1, at=b, labels=subdat$Gear_level1, las=3, cex.axis=2.5)
+  axis(2, las=1, cex.axis=2)
+  text(x=b, y=max(subdat$NrPaps)*0.035, labels=subdat$NrPaps, cex=1.5, font=3)
+  box()
+  if(iType==1){
+    axis(2, tick=F, at=(max(subdat$NrPaps)*1.05)/2, labels="Number of papers",
+         line=4.5, cex.axis=3)}
+}
+par(fig=c(0,1,0,1), new=TRUE)
+par(mar=c(2,2,6,2))
+plot(c(0,1), c(0,1), type="n", axes=F, ann=F)
+# title("The different fishing gears studied", line=0, cex.main=3, font.main=2)
+dev.off()
+
+
 #####################################################################################################################-
 #####################################################################################################################-
 #-----------------------------------------------#
@@ -59,19 +209,19 @@ Regions                               <- data[, .(NrPaps = length(unique(SW.ID))
 Regions                               <- Regions[, .(NrPaps = sum(NrPaps)), by="Region"]
 Regions                               <- Regions[order(NrPaps, decreasing = F),,]
 
-sum(Regions$NrPaps) #566, suggesting that, out of 549 papers, several of them studied multiple regions
+sum(Regions$NrPaps) #558, suggesting that, out of 549 papers, several of them studied multiple regions
 
 
 tiff(paste0(outPath, "Regions.tiff"), width=1000, height=750, res=100)
-par(mar=c(5, 12, 4, 2))
-b                                     <- barplot(Regions$NrPaps, horiz=TRUE, axes=F, xlim=c(0,max(Regions$NrPaps+20)))
+par(mar=c(5, 15, 4, 2))
+b                                     <- barplot(Regions$NrPaps, horiz=TRUE, axes=F, xlim=c(0,max(Regions$NrPaps+20)), col=viridis(3)[2])
 box()
-axis(2, at=b, labels=Regions$Region, las=1, cex.axis=1.2)
-axis(1, at= seq(0,max(Regions$NrPaps), 25), labels=seq(0, max(Regions$NrPaps), 25), cex.axis=1.2)
+axis(2, at=b, labels=Regions$Region, las=1, cex.axis=2)
+axis(1, at= seq(0,max(Regions$NrPaps), 50), labels=seq(0, max(Regions$NrPaps), 50), cex.axis=1.5)
 # title(main="Number of papers per region", cex.main=1.5, font.main=2)
-text(x=180, y=0, paste0("Total number of unique papers ", length(unique(data$SW.ID))), pos=4)
-text(x=Regions$NrPaps + 8, y=b, Regions$NrPaps)
-axis(1, at=max(Regions$NrPaps)/2, tick=F, line=2, label="Number of papers", cex.axis=1.3)
+text(x=175, y=0, paste0("Total number of unique papers ", length(unique(data$SW.ID))), pos=4)
+text(x=Regions$NrPaps + 9, y=b, Regions$NrPaps, cex=1.3)
+axis(1, at=max(Regions$NrPaps)/2, tick=F, line=2, label="Number of papers", cex.axis=2)
 dev.off()
 
 
@@ -300,7 +450,7 @@ ggsave("PressureYear.tiff", p, path=outPath, width=10, height = 5)
 #####################################################################################################################-
 #####################################################################################################################-
 #-----------------------------------------------#
-# Sankey diagrma ----
+# Sankey diagram ----
 #-----------------------------------------------#
 
 #-----------------------------------------------#
@@ -316,6 +466,8 @@ sankeyDat                            <- sankeyDat[!duplicated(sankeyDat),]
 
 sankeyDat$Fishery.type               <- factor(sankeyDat$Fishery.type, levels = c("Unknown","Scientific","Recreational","Commercial"))
 
+sankeyDat$Response.variable_category <- with(sankeyDat, ifelse(Response.variable_category %in% "Survival","Mortality",Response.variable_category))  
+
 sankeyInput                          <- make_long(sankeyDat, Fishery.type,Pressure.type,Ecosystem.component_level1,Response.variable_category)
 
 
@@ -324,7 +476,8 @@ sankeyInput                          <- make_long(sankeyDat, Fishery.type,Pressu
 #====-
 ## Set colors
 colorpal                             <- brewer.pal(8,"Paired")[-c(2,4,6,8)]
-mycolors                             <- c(colorpal[1],4), rep(colorpal[3],7), rep(colorpal[5],11), rep(colorpal[7],13))
+colorpal                             <- viridis(8)[-c(1,2,3,6)]
+# mycolors                             <- c(rep(colorpal[1],4), rep(colorpal[3],7), rep(colorpal[5],11), rep(colorpal[7],13))
 
 ## Build sankey
 sankey <- ggplot(sankeyInput,
@@ -334,7 +487,7 @@ sankey <- ggplot(sankeyInput,
                                next_node = next_node,
                                fill = factor(x),
                                label = node)) +
-  scale_x_discrete(labels=c("Fishery","Pressure","Ecosystem component","Aspect")) +
+  scale_x_discrete(labels=c("Fishery","Pressure","Ecosystem component","Impact")) +
   scale_fill_manual(values=colorpal) +
   geom_sankey(flow.fill="grey",
               flow.alpha=0.8) +
@@ -342,7 +495,7 @@ sankey <- ggplot(sankeyInput,
   theme_few()+
   theme(axis.title = element_blank(),
         axis.text.y = element_blank(),
-        axis.text = element_text(size=20),
+        axis.text = element_text(size=20, colour = "black"),
         legend.position = "none")
 
 ## View Sankey
@@ -356,6 +509,61 @@ ggsave("Sankey.tiff", sankey, path=outPath,
 
 ## View interactive sankey (needs work)
 ggplotly(sankey)
+
+
+
+#-----------------------------------------------#
+## Fisheries-Gear-Pressure-Ecosystem component-Aspect -----
+#-----------------------------------------------#
+
+#===-
+# Data cleaning
+#====-
+## Create long version
+sankeyDat                            <- data[,c("SW.ID","Fishery.type","Pressure.type","Gear_level1","Ecosystem.component_level1","Response.variable_category")]
+sankeyDat$Gear_level1                <- with(sankeyDat, ifelse(is.na(Gear_level1),"Unknown",Gear_level1))
+sankeyDat                            <- sankeyDat[!duplicated(sankeyDat),]
+
+sankeyDat$Fishery.type               <- factor(sankeyDat$Fishery.type, levels = c("Unknown","Scientific","Recreational","Commercial"))
+
+sankeyDat$Response.variable_category <- with(sankeyDat, ifelse(Response.variable_category %in% "Survival","Mortality",Response.variable_category))  
+
+sankeyInput                          <- make_long(sankeyDat, Fishery.type,Gear_level1,Pressure.type,Ecosystem.component_level1,Response.variable_category)
+
+
+#===-
+# Create sankey diagram
+#====-
+## Set colors
+colorpal                             <- viridis(8)[-c(1,2,3)]
+
+## Build sankey
+sankey <- ggplot(sankeyInput,
+                 mapping = aes(x = x,
+                               next_x = next_x,
+                               node = node,
+                               next_node = next_node,
+                               fill = factor(x),
+                               label = node)) +
+  scale_x_discrete(labels=c("Fishery","Gear","Pressure","Ecosystem component","Impact")) +
+  scale_fill_manual(values=colorpal) +
+  geom_sankey(flow.fill="grey",
+              flow.alpha=0.8) +
+  geom_sankey_label(size=7) +
+  theme_few()+
+  theme(axis.title = element_blank(),
+        axis.text.y = element_blank(),
+        axis.text = element_text(size=20, colour = "black"),
+        legend.position = "none")
+
+## View Sankey
+sankey
+
+## Save sankey
+ggsave("Sankey5.tiff", sankey, path=outPath,
+       width = 400,
+       height = 200,
+       units = "mm")
 
 
 
