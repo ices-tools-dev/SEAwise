0ffa7fb54ace427cb935fa02b0d1349434f4f8e0
Elliot John Brown <elbr@dtu.dk>
2022-11-07 21:14:38 +0100

    Task 1.2 scoping recoding

---
+++ Systematic Reviews/Analysis Task 1.2/Scripts/ScopingExploratoryAnalyses.R
@@ -0,0 +1,28 @@
+#
+##
+### This script explores the raw scoping input from various stakeholders from the first round of SEAwise workshops
+### Analyses by Elliot J. Brown 
+##
+#
+
+#===
+# Dependencies ----
+#===
+
+#====
+
+#===
+# Data ----
+rd <- read.csv(file = "Systematic Reviews/Analysis Task 1.2/Databases/coding_all_09052022.csv", header = T)
+#====
+
+#===
+# Data Cleaning ----
+
+#====
+
+#===
+# Coding and matching categories ----
+#===
+
+#====
