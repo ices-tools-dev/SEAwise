233e0f1ab87d85f487b4f9a9764708bf82229fdd
Elliot John Brown <elbr@dtu.dk>
2023-01-31 08:11:44 +0100

    beginning synthesis analyses

--- Systematic Reviews/Analysis Task 1.2/Scripts/ScopingExploratoryAnalyses.R
+++ Systematic Reviews/Analysis Task 1.2/Scripts/ScopingExploratoryAnalyses.R
@@ -18,11 +18,20 @@ rd <- read.csv(file = "Systematic Reviews/Analysis Task 1.2/Databases/coding_all
 
 #===
 # Data Cleaning ----
+#===
 
 #====
 
 #===
 # Coding and matching categories ----
 #===
+
+#====
+
+#===
+# Temp figures for task 6.1 ----
+#===
+measures <- c("share of sea by different activities", "management", "governance", "spatial management", "tools", "spatially explicit", "delayed advice", "regulation", "Landing obligation", "regulation incoherence", "implementation", "collaboration", "quota exchange", "LTMS", "nature restoration", "control and enforcement", "management framework", "energy islands", "balanced harvesting", "market tools", "temporal closures ", "catch limits", "co-management", "displacement ", "NOT landing obligation", "selective gear", "spatial / temporal restrictions ", "coastal development", "management strategies", "fisher's autonomy", "fishing new stocks", "multispecies management", "short and long term temporal fisheries management", "spatio-temporal restrictions", "species catch options", "selfmanagment", "national quota allocation", "multispecies MEY", "compliance", "no-take zones", "ecosystem based management", "regulation measures", "Fmsy", "Multiannual Plans", "ecosystem based fisheries management", "management strategy", "adaptive management", "multispecies management ", "management objectoves", "spawning closures", "bag limit", "multi-stakeholder approach", "documentation of spawning closures", "culling", "effective control", "fleet overcapacity", "third country fleets", "temporal closures", "effort displacement", "protection measures", "closures", "fishing closure", "spill-over effects", "gear regulations", "other spatial and temporal measures", "management tools" , "decisive power back to fishers", "protection (recovery)", "spatial activity managing", "market price dynamics", "technical interactions", "alternative fishing grounds", "EBFM", "management measures", "landing obligation", "ecosystem approach for management ", "TAC", "NOT minimum mesh size", "labour standards", "access", "infrastructure", "OECM (other effective conservation measures)", "marine spatial planning")
+measures <- measures[order(measures)]
 
 #====
